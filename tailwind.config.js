bed241157eca370b1ccdef02640cfde514cc9218
yuchulK <warabeat_mell@yahoo.co.jp>
2022-01-11 11:16:37 +0900

    修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+const cssnano = require('cssnano')
 module.exports = {
-  content: ["./dist/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+        cssnano({
+            preset: 'default'
+        }),
+        purgecss({
+            content: ['./dist/index.html', './dist/fc_referral.html'],
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+        })
+    ]
 }

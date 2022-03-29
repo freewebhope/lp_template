547a94b90896dcd20b710fe539bd8c78ba5c1fc3
shotakamei1023 <kameishota1023@gmail.com>
2022-03-29 10:03:35 +0900

    css追加 css minify停止

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,14 @@
 const purgecss = require('@fullhuman/postcss-purgecss')
-const cssnano = require('cssnano')
+// const cssnano = require('cssnano')
 module.exports = {
     plugins: [
         require('tailwindcss'),
         require('autoprefixer'),
-        cssnano({
-            preset: 'default'
-        }),
+        // cssnano({
+        //     preset: 'default'
+        // }),
         purgecss({
-            content: ['./dist/index.html'],
+            content: ['./dist/index.html','./dist/form.html'],
             defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
         })
     ]
